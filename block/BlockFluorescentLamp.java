f6b49b5de0f2aa2fb7ac67d6e0619d7567d6ab1e
EnkeyMC <enkeymc@gmail.com>
2013-12-20 18:14:57 +0100

    Finished Fluorescent Lamp

--- block/BlockFluorescentLamp.java
+++ block/BlockFluorescentLamp.java
@@ -1,18 +1,22 @@
 package morelights.block;
 
+import static net.minecraftforge.common.ForgeDirection.DOWN;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import morelights.lib.Pix;
 import morelights.lib.Reference;
 import morelights.proxy.ClientProxy;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.world.World;
 
 public class BlockFluorescentLamp extends Block{
 
 	public BlockFluorescentLamp(int par1, Material par2Material) {
 		super(par1, par2Material);
 		this.setLightValue(1F);
+		this.setBlockBounds(Pix.FOUR, Pix.FOURTEEN, Pix.FOUR, Pix.TWELVE, Pix.SIXTEEN, Pix.TWELVE);
 	}
 	
 	@Override
@@ -30,6 +34,20 @@ public class BlockFluorescentLamp extends Block{
 	public boolean renderAsNormalBlock(){
 		return false;
 	}
+	
+	@Override
+	public void onNeighborBlockChange(World par1World, int par2, int par3, int par4, int par5) {
+		if(!this.canPlaceBlockAt(par1World, par2, par3, par4)){
+			par1World.setBlockToAir(par2, par3, par4);
+			this.dropBlockAsItem(par1World, par2, par3, par4, 0, 0);
+		}
+	}
+	
+	@Override
+	public boolean canPlaceBlockAt(World par1World, int par2, int par3, int par4)
+    {		
+        return par1World.isBlockSolidOnSide(par2, par3 + 1, par4, DOWN);
+    }
 	
 	@Override
 	@SideOnly(Side.CLIENT)
--- renderers/FluorescentLampRenderer.java
+++ renderers/FluorescentLampRenderer.java
@@ -1,5 +1,7 @@
 package morelights.renderers;
 
+import org.lwjgl.opengl.GL11;
+
 import morelights.lib.Pix;
 import morelights.proxy.ClientProxy;
 import net.minecraft.block.Block;
@@ -15,7 +17,49 @@ public class FluorescentLampRenderer implements ISimpleBlockRenderingHandler {
 	@Override
 	public void renderInventoryBlock(Block block, int metadata, int modelID,
 			RenderBlocks renderer) {
-
+		Tessellator tes = Tessellator.instance;
+		 
+	    tes.startDrawingQuads();
+	    
+	    tes.addTranslation(-0.3F, 0, -0.3F);
+		
+		int x = 0;
+		int y = 0;
+		int z = 0;
+		
+		float offset = -0.02F;
+				
+		Icon icon = block.getIcon(0, 0);
+		
+		tes.setNormal(0.0F, 1.0F, 0.0F);
+		
+		EasyTess.renderCube(x + Pix.FOUR, y, z + Pix.FOUR, 8, 1, 8, 0, 8, icon, 1, 32, 32);
+		
+		EasyTess.renderCube(x + Pix.FOUR, y + Pix.ONE, z + Pix.FOUR, 1, 1, 1, 4, 25, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.FOUR, y + Pix.ONE, z + Pix.ELEVEN, 1, 1, 1, 9, 20, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.ELEVEN, y + Pix.ONE, z + Pix.FOUR, 1, 1, 1, 0, 18, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.ELEVEN, y + Pix.ONE, z + Pix.ELEVEN, 1, 1, 1, 6, 22, icon, 1, 32, 32);
+		
+		EasyTess.renderCube(x + Pix.SIX, y + Pix.ONE + offset, z + Pix.SIX, 1, 1, 1, 16, 0, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.NINE, y + Pix.ONE + offset, z + Pix.SIX, 1, 1, 1, 16, 0, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.SIX, y + Pix.ONE + offset, z + Pix.NINE, 1, 1, 1, 16, 0, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.NINE, y + Pix.ONE + offset, z + Pix.NINE, 1, 1, 1, 16, 0, icon, 1, 32, 32);
+		
+		EasyTess.renderCube(x + Pix.SEVEN, y + Pix.ONE + offset, z + Pix.SIX, 2, 1, 1, 18, 5, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.SIX, y + Pix.ONE + offset, z + Pix.SEVEN, 1, 1, 2, 26, 0, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.SEVEN, y + Pix.ONE + offset, z + Pix.NINE, 2, 1, 1, 20, 9, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.NINE, y + Pix.ONE + offset, z + Pix.SEVEN, 1, 1, 2, 18, 0, icon, 1, 32, 32);
+		
+		EasyTess.renderCube(x + Pix.FIVE, y + Pix.TWO, z + Pix.FIVE, 6, 0, 6, 16, 16, icon, 0, 32, 32);
+		
+		EasyTess.renderCube(x + Pix.ELEVEN, y + Pix.ONE, z + Pix.FIVE, 1, 1, 6, 3, 16, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.FOUR, y + Pix.ONE, z + Pix.FIVE, 1, 1, 6, 8, 18, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.FIVE, y + Pix.ONE, z + Pix.ELEVEN, 6, 1, 1, 2, 20, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.FIVE, y + Pix.ONE, z + Pix.FOUR, 6, 1, 1, 5, 16, icon, 1, 32, 32);
+		
+		tes.addTranslation(0.3F, 0, 0.3F);
+		
+	    tes.draw();
 	}
 
 	@Override
@@ -27,63 +71,90 @@ public class FluorescentLampRenderer implements ISimpleBlockRenderingHandler {
 		tes.setColorOpaque_F(1.0F, 1.0F, 1.0F);
 				
 		Icon icon = block.getIcon(0, 0);
+		double offset = 0.02;
+		boolean connected = false;
+		
+		int bl = world.getBlockId(x + 1, y, z); 
+		if(bl == block.blockID){
+			connected = true;
+			//side 2
+			EasyTess.renderCube(x + Pix.TWELVE, y + Pix.FIFTEEN, z + Pix.FOUR, 4, 1, 8, 5, 8, icon, 1, 32, 32);
+			EasyTess.renderCube(x + Pix.TWELVE, y + Pix.FOURTEEN, z + Pix.FOUR, 4, 1, 1, 8, 26, icon, 1, 32, 32);
+			EasyTess.renderCube(x + Pix.TWELVE, y + Pix.FOURTEEN, z + Pix.ELEVEN, 4, 1, 1, 9, 16, icon, 1, 32, 32);
+			EasyTess.renderCube(x + Pix.TEN, y + Pix.FOURTEEN + offset, z + Pix.SIX, 6, 1, 1, 20, 8, icon, 1, 32, 32);
+			EasyTess.renderCube(x + Pix.TEN, y + Pix.FOURTEEN + offset, z + Pix.NINE, 6, 1, 1, 26, 7, icon, 1, 32, 32);
+			EasyTess.renderCube(x + Pix.ELEVEN, y + Pix.FOURTEEN, z + Pix.FIVE, 5, 0, 6, 27, 16, icon, 0, 32, 32);
+		}else{
+			EasyTess.renderCube(x + Pix.ELEVEN, y + Pix.FOURTEEN, z + Pix.FIVE, 1, 1, 6, 3, 16, icon, 1, 32, 32);
+		}
+		
+		bl = world.getBlockId(x - 1, y, z);
+		if(bl == block.blockID){
+			connected = true;
+			//side 1
+			EasyTess.renderCube(x, y + Pix.FIFTEEN, z + Pix.FOUR, 4, 1, 8, 8, 8, icon, 1, 32, 32);
+			EasyTess.renderCube(x, y + Pix.FOURTEEN, z + Pix.FOUR, 4, 1, 1, 0, 26, icon, 1, 32, 32);
+			EasyTess.renderCube(x, y + Pix.FOURTEEN, z + Pix.ELEVEN, 4, 1, 1, 10, 27, icon, 1, 32, 32);
+			EasyTess.renderCube(x, y + Pix.FOURTEEN + offset, z + Pix.SIX, 6, 1, 1, 22, 9, icon, 1, 32, 32);
+			EasyTess.renderCube(x, y + Pix.FOURTEEN + offset, z + Pix.NINE, 6, 1, 1, 19, 6, icon, 1, 32, 32);
+			EasyTess.renderCube(x, y + Pix.FOURTEEN, z + Pix.FIVE, 5, 0, 6, 22, 16, icon, 0, 32, 32);
+		}else{
+			EasyTess.renderCube(x + Pix.FOUR, y + Pix.FOURTEEN, z + Pix.FIVE, 1, 1, 6, 8, 18, icon, 1, 32, 32);
+		}
+		
+		bl = world.getBlockId(x, y, z + 1);
+		if(bl == block.blockID){
+			connected = true;
+			//side 4
+			EasyTess.renderCube(x + Pix.FOUR, y + Pix.FIFTEEN, z + Pix.TWELVE, 8, 1, 4, 0, 12, icon, 1, 32, 32); 
+			EasyTess.renderCube(x + Pix.FOUR, y + Pix.FOURTEEN, z + Pix.TWELVE, 1, 1, 4, 9, 16, icon, 1, 32, 32);
+			EasyTess.renderCube(x + Pix.ELEVEN, y + Pix.FOURTEEN, z + Pix.TWELVE, 1, 1, 4, 2, 21, icon, 1, 32, 32);
+			EasyTess.renderCube(x + Pix.SIX, y + Pix.FOURTEEN + offset, z + Pix.TEN, 1, 1, 6, 20, 6, icon, 1, 32, 32);
+			EasyTess.renderCube(x + Pix.NINE, y + Pix.FOURTEEN + offset, z + Pix.TEN, 1, 1, 6, 19, 4, icon, 1, 32, 32);
+			EasyTess.renderCube(x + Pix.FIVE, y + Pix.FOURTEEN, z + Pix.ELEVEN, 6, 0, 5, 22, 22, icon, 0, 32, 32);
+		}else{
+			EasyTess.renderCube(x + Pix.FIVE, y + Pix.FOURTEEN, z + Pix.ELEVEN, 6, 1, 1, 2, 20, icon, 1, 32, 32);
+		}
+		
+		bl = world.getBlockId(x, y, z - 1);
+		if(bl == block.blockID){
+			connected = true;
+			//side 3
+			EasyTess.renderCube(x + Pix.FOUR, y + Pix.FIFTEEN, z, 8, 1, 4, 0, 12, icon, 1, 32, 32); 
+			EasyTess.renderCube(x + Pix.FOUR, y + Pix.FOURTEEN, z, 1, 1, 4, 3, 18, icon, 1, 32, 32);
+			EasyTess.renderCube(x + Pix.ELEVEN, y + Pix.FOURTEEN, z, 1, 1, 4, 4, 20, icon, 1, 32, 32);
+			EasyTess.renderCube(x + Pix.SIX, y + Pix.FOURTEEN + offset, z, 1, 1, 6, 18, 8, icon, 1, 32, 32);
+			EasyTess.renderCube(x + Pix.NINE, y + Pix.FOURTEEN + offset, z, 1, 1, 6, 24, 2, icon, 1, 32, 32);
+			EasyTess.renderCube(x + Pix.FIVE, y + Pix.FOURTEEN, z, 6, 0, 5, 16, 22, icon, 0, 32, 32);
+		}else{
+			EasyTess.renderCube(x + Pix.FIVE, y + Pix.FOURTEEN, z + Pix.FOUR, 6, 1, 1, 5, 16, icon, 1, 32, 32);
+		}
 		
 		//Base
-		EasyTess.renderCube(x + Pix.FOUR, y + Pix.FIFTEEN, z + Pix.FOUR, 8, 1, 8, 0, 4, icon, 0, 32, 32);
+		EasyTess.renderCube(x + Pix.FOUR, y + Pix.FIFTEEN, z + Pix.FOUR, 8, 1, 8, 0, 8, icon, 1, 32, 32);
 		
-		EasyTess.renderCube(x + Pix.FOUR, y + Pix.FOURTEEN, z + Pix.FOUR, 1, 1, 1, 4, 9, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.FOUR, y + Pix.FOURTEEN, z + Pix.ELEVEN, 1, 1, 1, 9, 4, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.ELEVEN, y + Pix.FOURTEEN, z + Pix.FOUR, 1, 1, 1, 0, 10, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.ELEVEN, y + Pix.FOURTEEN, z + Pix.ELEVEN, 1, 1, 1, 6, 2, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.FOUR, y + Pix.FOURTEEN, z + Pix.FOUR, 1, 1, 1, 4, 25, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.FOUR, y + Pix.FOURTEEN, z + Pix.ELEVEN, 1, 1, 1, 9, 20, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.ELEVEN, y + Pix.FOURTEEN, z + Pix.FOUR, 1, 1, 1, 0, 18, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.ELEVEN, y + Pix.FOURTEEN, z + Pix.ELEVEN, 1, 1, 1, 6, 22, icon, 1, 32, 32);
 		
-		EasyTess.renderCube(x + Pix.SIX, y + Pix.FOURTEEN, z + Pix.SIX, 1, 1, 1, 16, 0, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.NINE, y + Pix.FOURTEEN, z + Pix.SIX, 1, 1, 1, 16, 0, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.SIX, y + Pix.FOURTEEN, z + Pix.NINE, 1, 1, 1, 16, 0, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.NINE, y + Pix.FOURTEEN, z + Pix.NINE, 1, 1, 1, 16, 0, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.SIX, y + Pix.FOURTEEN + offset, z + Pix.SIX, 1, 1, 1, 16, 0, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.NINE, y + Pix.FOURTEEN + offset, z + Pix.SIX, 1, 1, 1, 16, 0, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.SIX, y + Pix.FOURTEEN + offset, z + Pix.NINE, 1, 1, 1, 16, 0, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.NINE, y + Pix.FOURTEEN + offset, z + Pix.NINE, 1, 1, 1, 16, 0, icon, 1, 32, 32);
 		
-		EasyTess.renderCube(x + Pix.SEVEN, y + Pix.FOURTEEN, z + Pix.SIX, 2, 1, 1, 18, 5, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.SIX, y + Pix.FOURTEEN, z + Pix.SEVEN, 1, 1, 2, 26, 0, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.SEVEN, y + Pix.FOURTEEN, z + Pix.NINE, 2, 1, 1, 20, 9, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.NINE, y + Pix.FOURTEEN, z + Pix.SEVEN, 1, 1, 2, 18, 0, icon, 1, 32, 32);
-		//When not connected
-		EasyTess.renderCube(x + Pix.FOUR, y + Pix.FOURTEEN, z + Pix.FIVE, 1, 1, 6, 8, 2, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.FIVE, y + Pix.FOURTEEN, z + Pix.FOUR, 6, 1, 1, 5, 0, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.ELEVEN, y + Pix.FOURTEEN, z + Pix.FIVE, 1, 1, 6, 3, 0, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.FIVE, y + Pix.FOURTEEN, z + Pix.ELEVEN, 6, 1, 1, 2, 4, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.SEVEN, y + Pix.FOURTEEN + offset, z + Pix.SIX, 2, 1, 1, 18, 5, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.SIX, y + Pix.FOURTEEN + offset, z + Pix.SEVEN, 1, 1, 2, 26, 0, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.SEVEN, y + Pix.FOURTEEN + offset, z + Pix.NINE, 2, 1, 1, 20, 9, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.NINE, y + Pix.FOURTEEN + offset, z + Pix.SEVEN, 1, 1, 2, 18, 0, icon, 1, 32, 32);
 		
-		//side 1
-		EasyTess.renderCube(x, y + Pix.FIFTEEN, z + Pix.FOUR, 4, 1, 8, 8, 4, icon, 1, 32, 32);
-		EasyTess.renderCube(x, y + Pix.FOURTEEN, z + Pix.FOUR, 4, 1, 1, 0, 10, icon, 1, 32, 32);
-		EasyTess.renderCube(x, y + Pix.FOURTEEN, z + Pix.ELEVEN, 4, 1, 1, 10, 8, icon, 1, 32, 32);
-		EasyTess.renderCube(x, y + Pix.FOURTEEN, z + Pix.SIX, 6, 1, 1, 22, 9, icon, 1, 32, 32);
-		EasyTess.renderCube(x, y + Pix.FOURTEEN, z + Pix.NINE, 6, 1, 1, 19, 6, icon, 1, 32, 32);
-		
-		//side 2
-		EasyTess.renderCube(x + Pix.TWELVE, y + Pix.FIFTEEN, z + Pix.FOUR, 4, 1, 8, 5, 5, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.TWELVE, y + Pix.FOURTEEN, z + Pix.FOUR, 4, 1, 1, 8, 8, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.TWELVE, y + Pix.FOURTEEN, z + Pix.ELEVEN, 4, 1, 1, 9, 0, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.TEN, y + Pix.FOURTEEN, z + Pix.SIX, 6, 1, 1, 20, 8, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.TEN, y + Pix.FOURTEEN, z + Pix.NINE, 6, 1, 1, 26, 7, icon, 1, 32, 32);
-		
-		//side 3
-		EasyTess.renderCube(x + Pix.FOUR, y + Pix.FIFTEEN, z, 8, 1, 4, 0, 3, icon, 1, 32, 32); 
-		EasyTess.renderCube(x + Pix.FOUR, y + Pix.FOURTEEN, z, 1, 1, 4, 3, 0, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.ELEVEN, y + Pix.FOURTEEN, z, 1, 1, 4, 4, 4, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.SIX, y + Pix.FOURTEEN, z, 1, 1, 6, 18, 8, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.NINE, y + Pix.FOURTEEN, z, 1, 1, 6, 24, 2, icon, 1, 32, 32);
-		
-		//side 4
-		EasyTess.renderCube(x + Pix.FOUR, y + Pix.FIFTEEN, z + Pix.TWELVE, 8, 1, 4, 0, 3, icon, 1, 32, 32); 
-		EasyTess.renderCube(x + Pix.FOUR, y + Pix.FOURTEEN, z + Pix.TWELVE, 1, 1, 4, 9, 0, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.ELEVEN, y + Pix.FOURTEEN, z + Pix.TWELVE, 1, 1, 4, 2, 5, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.SIX, y + Pix.FOURTEEN, z + Pix.TEN, 1, 1, 6, 20, 6, icon, 1, 32, 32);
-		EasyTess.renderCube(x + Pix.NINE, y + Pix.FOURTEEN, z + Pix.TEN, 1, 1, 6, 19, 4, icon, 1, 32, 32);
+		EasyTess.renderCube(x + Pix.FIVE, y + Pix.FOURTEEN, z + Pix.FIVE, 6, 0, 6, 16, 16, icon, 0, 32, 32);
 		return true;
 	}
 
 	@Override
 	public boolean shouldRender3DInInventory() {
-		return false;
+		return true;
 	}
 
 	@Override
